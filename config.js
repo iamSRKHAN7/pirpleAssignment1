896cf8f381795f1172d256f66b2cd4aebc03ca89
saekhan <saeedurrahman530@gmail.com>
2018-08-14 19:08:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+﻿
+var environments = {};
+
+environments.staging = {
+    'httpPort': 1234,
+    'envName': 'staging'
+};
+environments.production = {
+    'httpPort': 4321,
+    'envName': 'production'
+};
+
+var currentEnv = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV : '';
+var envToExport = typeof(environments[currentEnv]) == 'object' ? environments[currentEnv] : environments.staging;
+
+module.exports = envToExport;
---
+++ index.js
@@ -0,0 +1,73 @@
+﻿
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+
+var httpServer = http.createServer(function(req, res) {
+    console.log('/***Request received***/');
+
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    var queryStringObject = parsedUrl.query;
+    console.log('Path: '+trimmedPath);
+    console.log('Query String: ',queryStringObject);
+
+    var method = req.method.toLocaleLowerCase();
+    console.log('Method: '+method);
+
+    var headers = req.headers;
+    console.log('Heasers: ',headers);
+
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+        console.log('Payload: '+buffer);
+		
+		var chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+		
+		var data = {
+			'trimmedPath' : trimmedPath,
+			'queryStringObject' : queryStringObject,
+			'method' : method,
+			'headers' : headers,
+			'payload' : buffer
+		};
+
+        chooseHandler(data, function(statusCode, payload) {
+			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+			payload = typeof(payload) == 'object' ? payload : {};
+			
+			var payloadString = JSON.stringify(payload);
+			console.log('payload: '+payloadString);
+			res.setHeader('Content-Type', 'application/json');
+			res.writeHead(statusCode);
+			res.end(payloadString);
+			//res.end('End of Server\n');
+		});
+    }); 
+});
+
+httpServer.listen(config.httpPort, function() {
+    console.log('/***Http listener***/');
+    console.log('PORT: '+config.httpPort);
+    console.log('ENV: '+config.envName);
+}); 
+
+var handlers = {};
+
+handlers.hello = function(data, callback) {
+    callback(202, {'message': 'hello'})
+};
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+var router = {
+    'hello': handlers.hello
+};
